6e2752b84e7c2c437403d3d71c84b25fde02491d
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2019-01-12 09:54:42 +0530

    Deleted UIRectEdge and replaced edges with attributes

--- AutoLayoutProxy/UIViewExtension.swift
+++ AutoLayoutProxy/UIViewExtension.swift
@@ -18,31 +18,31 @@ public extension UIView {
         closure(LayoutProxy(view: view, superView: self))
     }
     
-    public func addSubview(_ view: UIView, edges: [UIRectEdge] = [], top: NSLayoutYAxisAnchor? = nil, leading: NSLayoutXAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, trailing: NSLayoutXAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, padding: UIEdgeInsets = .zero, centerX: NSLayoutXAxisAnchor? = nil, centerY: NSLayoutYAxisAnchor? = nil, size: CGSize = .zero) {
+    public func add(_ view: UIView, attributes: [NSLayoutConstraint.Attribute], top: NSLayoutYAxisAnchor? = nil, leading: NSLayoutXAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, trailing: NSLayoutXAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, padding: UIEdgeInsets = .zero, centerX: NSLayoutXAxisAnchor? = nil, centerY: NSLayoutYAxisAnchor? = nil, size: CGSize = .zero) {
         
         addSubview(view, layout: {
-            if edges.contains(.top) || top != nil {
+            if attributes.contains(.top) || top != nil {
                 $0.top == (top ?? topAnchor) + padding.top
             }
-            if edges.contains(.left) || left != nil {
+            if attributes.contains(.left) || left != nil {
                 $0.left == (left ?? leftAnchor) + padding.left
             }
-            if edges.contains(.leading) || leading != nil {
+            if attributes.contains(.leading) || leading != nil {
                 $0.leading == (leading ?? leadingAnchor) + padding.leading
             }
-            if edges.contains(.bottom) || bottom != nil {
+            if attributes.contains(.bottom) || bottom != nil {
                 $0.bottom == (bottom ?? bottomAnchor) + padding.bottom
             }
-            if edges.contains(.right) || right != nil {
+            if attributes.contains(.right) || right != nil {
                 $0.right == (right ?? rightAnchor) + padding.right
             }
-            if edges.contains(.trailing) || trailing != nil {
+            if attributes.contains(.trailing) || trailing != nil {
                 $0.trailing == (trailing ?? trailingAnchor) + padding.trailing
             }
-            if edges.contains(.centerX) || centerX != nil {
+            if attributes.contains(.centerX) || centerX != nil {
                 $0.centerX == (centerX ?? centerXAnchor) + padding.centerX
             }
-            if edges.contains(.centerY) || centerY != nil {
+            if attributes.contains(.centerY) || centerY != nil {
                 $0.centerY == (centerY ?? centerYAnchor) + padding.centerY
             }
             if size != .zero {
