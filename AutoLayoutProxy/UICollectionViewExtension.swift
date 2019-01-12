eda57ca944690969a17b518577939927798a96d7
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2019-01-12 09:53:57 +0530

    Changed UITableViewCell and UICollectionViewCell to NSObject for register and dequeue

--- AutoLayoutProxy/UICollectionViewExtension.swift
+++ AutoLayoutProxy/UICollectionViewExtension.swift
@@ -8,11 +8,11 @@
 
 public extension UICollectionView {
     
-    public func register<CellClass: UICollectionViewCell>(_ cell: CellClass.Type) {
+    public func register<CellClass: NSObject>(_ cell: CellClass.Type) {
         register(cell, forCellWithReuseIdentifier: cell.identifier)
     }
     
-    public func dequeue<CellClass: UICollectionViewCell>(_ class: CellClass.Type, for indexPath: IndexPath, configure: ((CellClass) -> Void)) -> UICollectionViewCell {
+    public func dequeue<CellClass: NSObject>(_ class: CellClass.Type, for indexPath: IndexPath, configure: ((CellClass) -> Void)) -> UICollectionViewCell {
         let cell = dequeueReusableCell(withReuseIdentifier: CellClass.identifier, for: indexPath)
         if let cell = cell as? CellClass {
             configure(cell)
