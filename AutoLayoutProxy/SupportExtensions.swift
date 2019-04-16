1354424171ad4ff64b4601e94c6a03b3cb046b29
Bibin Jacob Pulickal <bibin.jacob@fingent.com>
2019-04-16 10:43:21 +0530

    Added few additional support extensions.

--- AutoLayoutProxy/SupportExtensions.swift
+++ AutoLayoutProxy/SupportExtensions.swift
@@ -11,6 +11,14 @@ public extension Set where Element == NSLayoutConstraint.Attribute {
     static var allSides: Set<NSLayoutConstraint.Attribute> {
         return [.top, .left, .bottom, .right]
     }
+
+    static var horizontal: Set<NSLayoutConstraint.Attribute> {
+        return [.left, .right]
+    }
+
+    static var vertical: Set<NSLayoutConstraint.Attribute> {
+        return [.top, .bottom]
+    }
     
     static var bothCenters: Set<NSLayoutConstraint.Attribute> {
         return [.centerX, .centerY]
@@ -25,11 +33,11 @@ public extension UIEdgeInsets {
     init(t: CGFloat = 0, l: CGFloat = 0, b: CGFloat = 0, r: CGFloat = 0) {
         self.init(top: t, left: l, bottom: b, right: r)
     }
-    init(all: CGFloat) {
+    init(_ all: CGFloat) {
         self.init(top: all, left: all, bottom: -all, right: -all)
     }
     init(horizontal: CGFloat = 0, vertical: CGFloat = 0) {
-        self.init(top: horizontal, left: vertical, bottom: -horizontal, right: -vertical)
+        self.init(top: vertical, left: horizontal, bottom: -vertical, right: -horizontal)
     }
 }
 
@@ -37,7 +45,7 @@ public extension UIOffset {
     init(h: CGFloat = 0, v: CGFloat = 0) {
         self.init(horizontal: h, vertical: v)
     }
-    init(both: CGFloat) {
+    init(_ both: CGFloat) {
         self.init(horizontal: both, vertical: both)
     }
 }
@@ -46,7 +54,7 @@ public extension CGSize {
     init(w: CGFloat = 0, h: CGFloat = 0) {
         self.init(width: w, height: h)
     }
-    init(both: CGFloat) {
+    init(_ both: CGFloat) {
         self.init(width: both, height: both)
     }
 }
