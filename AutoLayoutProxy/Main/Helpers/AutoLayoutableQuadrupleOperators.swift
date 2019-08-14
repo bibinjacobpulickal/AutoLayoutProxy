7543b8f3eee160451b5aa34da98edd66f1500dee
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2019-08-14 21:01:04 +0530

    Minor improvements in autolayoutable qudruple operators.

--- AutoLayoutProxy/Main/Helpers/AutoLayoutableQuadrupleOperators.swift
+++ AutoLayoutProxy/Main/Helpers/AutoLayoutableQuadrupleOperators.swift
@@ -7,24 +7,27 @@
 //
 
 
-public func ==(lhs: (NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, NSLayoutYAxisAnchor, NSLayoutXAxisAnchor),
-               rhs: (NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, NSLayoutYAxisAnchor, NSLayoutXAxisAnchor)) {
+public func ==<FirstAnchorType, SecondAnchorType, ThirdAnchorType, FourthAnchorType>(
+    lhs: (NSLayoutAnchor<FirstAnchorType>, NSLayoutAnchor<SecondAnchorType>, NSLayoutAnchor<ThirdAnchorType>, NSLayoutAnchor<FourthAnchorType>),
+    rhs: (NSLayoutAnchor<FirstAnchorType>, NSLayoutAnchor<SecondAnchorType>, NSLayoutAnchor<ThirdAnchorType>, NSLayoutAnchor<FourthAnchorType>)) {
     lhs.0.constraint(equalTo: rhs.0).isActive   = true
     lhs.1.constraint(equalTo: rhs.1).isActive   = true
     lhs.2.constraint(equalTo: rhs.2).isActive   = true
     lhs.3.constraint(equalTo: rhs.3).isActive   = true
 }
 
-public func <=(lhs: (NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, NSLayoutYAxisAnchor, NSLayoutXAxisAnchor),
-               rhs: (NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, NSLayoutYAxisAnchor, NSLayoutXAxisAnchor)) {
+public func <=<FirstAnchorType, SecondAnchorType, ThirdAnchorType, FourthAnchorType>(
+    lhs: (NSLayoutAnchor<FirstAnchorType>, NSLayoutAnchor<SecondAnchorType>, NSLayoutAnchor<ThirdAnchorType>, NSLayoutAnchor<FourthAnchorType>),
+    rhs: (NSLayoutAnchor<FirstAnchorType>, NSLayoutAnchor<SecondAnchorType>, NSLayoutAnchor<ThirdAnchorType>, NSLayoutAnchor<FourthAnchorType>)) {
     lhs.0.constraint(lessThanOrEqualTo: rhs.0).isActive   = true
     lhs.1.constraint(lessThanOrEqualTo: rhs.1).isActive   = true
     lhs.2.constraint(lessThanOrEqualTo: rhs.2).isActive   = true
     lhs.3.constraint(lessThanOrEqualTo: rhs.3).isActive   = true
 }
 
-public func >=(lhs: (NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, NSLayoutYAxisAnchor, NSLayoutXAxisAnchor),
-               rhs: (NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, NSLayoutYAxisAnchor, NSLayoutXAxisAnchor)) {
+public func >=<FirstAnchorType, SecondAnchorType, ThirdAnchorType, FourthAnchorType>(
+    lhs: (NSLayoutAnchor<FirstAnchorType>, NSLayoutAnchor<SecondAnchorType>, NSLayoutAnchor<ThirdAnchorType>, NSLayoutAnchor<FourthAnchorType>),
+    rhs: (NSLayoutAnchor<FirstAnchorType>, NSLayoutAnchor<SecondAnchorType>, NSLayoutAnchor<ThirdAnchorType>, NSLayoutAnchor<FourthAnchorType>)) {
     lhs.0.constraint(greaterThanOrEqualTo: rhs.0).isActive   = true
     lhs.1.constraint(greaterThanOrEqualTo: rhs.1).isActive   = true
     lhs.2.constraint(greaterThanOrEqualTo: rhs.2).isActive   = true
@@ -32,31 +35,34 @@ public func >=(lhs: (NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, NSLayoutYAxisAnch
 }
 
 
-public func +(lhs: (NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, NSLayoutYAxisAnchor, NSLayoutXAxisAnchor),
+public func +(lhs: (NSLayoutAnchor<NSLayoutYAxisAnchor>, NSLayoutAnchor<NSLayoutXAxisAnchor>, NSLayoutAnchor<NSLayoutYAxisAnchor>, NSLayoutAnchor<NSLayoutXAxisAnchor>),
               rhs: EdgeInsetConvertible)
-    -> (NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, EdgeInsetConvertible) {
+    -> (NSLayoutAnchor<NSLayoutYAxisAnchor>, NSLayoutAnchor<NSLayoutXAxisAnchor>, NSLayoutAnchor<NSLayoutYAxisAnchor>, NSLayoutAnchor<NSLayoutXAxisAnchor>, EdgeInsetConvertible) {
         return (lhs.0, lhs.1, lhs.2, lhs.3, rhs)
 }
 
 
-public func ==(lhs: (NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, NSLayoutYAxisAnchor, NSLayoutXAxisAnchor),
-               rhs: (NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, EdgeInsetConvertible)) {
+public func ==<FirstAnchorType, SecondAnchorType, ThirdAnchorType, FourthAnchorType>(
+    lhs: (NSLayoutAnchor<FirstAnchorType>, NSLayoutAnchor<SecondAnchorType>, NSLayoutAnchor<ThirdAnchorType>, NSLayoutAnchor<FourthAnchorType>),
+    rhs: (NSLayoutAnchor<FirstAnchorType>, NSLayoutAnchor<SecondAnchorType>, NSLayoutAnchor<ThirdAnchorType>, NSLayoutAnchor<FourthAnchorType>, EdgeInsetConvertible)) {
     lhs.0.constraint(equalTo: rhs.0, constant: rhs.4.top).isActive      = true
     lhs.1.constraint(equalTo: rhs.1, constant: rhs.4.left).isActive     = true
     lhs.2.constraint(equalTo: rhs.2, constant: rhs.4.bottom).isActive   = true
     lhs.3.constraint(equalTo: rhs.3, constant: rhs.4.right).isActive    = true
 }
 
-public func <=(lhs: (NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, NSLayoutYAxisAnchor, NSLayoutXAxisAnchor),
-               rhs: (NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, EdgeInsetConvertible)) {
+public func <=<FirstAnchorType, SecondAnchorType, ThirdAnchorType, FourthAnchorType>(
+    lhs: (NSLayoutAnchor<FirstAnchorType>, NSLayoutAnchor<SecondAnchorType>, NSLayoutAnchor<ThirdAnchorType>, NSLayoutAnchor<FourthAnchorType>),
+    rhs: (NSLayoutAnchor<FirstAnchorType>, NSLayoutAnchor<SecondAnchorType>, NSLayoutAnchor<ThirdAnchorType>, NSLayoutAnchor<FourthAnchorType>, EdgeInsetConvertible)) {
     lhs.0.constraint(lessThanOrEqualTo: rhs.0, constant: rhs.4.top).isActive    = true
     lhs.1.constraint(lessThanOrEqualTo: rhs.1, constant: rhs.4.left).isActive   = true
     lhs.2.constraint(lessThanOrEqualTo: rhs.2, constant: rhs.4.bottom).isActive = true
     lhs.3.constraint(lessThanOrEqualTo: rhs.3, constant: rhs.4.right).isActive  = true
 }
 
-public func >=(lhs: (NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, NSLayoutYAxisAnchor, NSLayoutXAxisAnchor),
-               rhs: (NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, NSLayoutYAxisAnchor, NSLayoutXAxisAnchor, EdgeInsetConvertible)) {
+public func >=<FirstAnchorType, SecondAnchorType, ThirdAnchorType, FourthAnchorType>(
+    lhs: (NSLayoutAnchor<FirstAnchorType>, NSLayoutAnchor<SecondAnchorType>, NSLayoutAnchor<ThirdAnchorType>, NSLayoutAnchor<FourthAnchorType>),
+    rhs: (NSLayoutAnchor<FirstAnchorType>, NSLayoutAnchor<SecondAnchorType>, NSLayoutAnchor<ThirdAnchorType>, NSLayoutAnchor<FourthAnchorType>, EdgeInsetConvertible)) {
     lhs.0.constraint(greaterThanOrEqualTo: rhs.0, constant: rhs.4.top).isActive     = true
     lhs.1.constraint(greaterThanOrEqualTo: rhs.1, constant: rhs.4.left).isActive    = true
     lhs.2.constraint(greaterThanOrEqualTo: rhs.2, constant: rhs.4.bottom).isActive  = true
