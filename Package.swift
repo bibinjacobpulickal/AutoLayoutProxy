e7ab664a025a4c5c0fb0e21104c08319dcb856d2
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2019-11-20 22:26:45 +0530

    Updated swift tool version 5.1 and added platforms.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "AutoLayoutProxy",
+    platforms: [.macOS(.v10_11), .iOS(.v9)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
